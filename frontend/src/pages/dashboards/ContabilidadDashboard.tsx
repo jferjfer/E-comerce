694c6153273bf08c0e053c8d352c5663a0c30238
Jose Fernando Vertel <jferjfer@gmail.com>
2026-04-18 21:42:04 -0500

    fix: corregir divs extra en ContabilidadDashboard - error TS1128

--- frontend/src/pages/dashboards/ContabilidadDashboard.tsx
+++ frontend/src/pages/dashboards/ContabilidadDashboard.tsx
@@ -858,10 +858,6 @@ export default function ContabilidadDashboard() {
           </div>
         )}
 
-            </div>
-          </div>
-        )}
-
         {/* Modal subir soporte */}
         {compraIdParaSoporte && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
